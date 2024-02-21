cf226cb92ba7fac4c34017997b4821330a72920f
ElzeeGo <lendor2211@gmail.com>
2024-02-21 20:22:12 +0200

    Remove branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,17 +50,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      Sukurta
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://inmedica.lt/'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> InMedica</span>
       </a>
     </span>
   );
